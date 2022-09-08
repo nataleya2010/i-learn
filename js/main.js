93339e69f905e6cd4ed2bb0b4844d86481d24679
nataleya2010 <gnwr2010@gmail.com>
2022-09-08 16:11:11 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log(1*5);
+const a = true;
+console.log(a);
+const ar = [];
+ar[0] = prompt('сколько вам лет?');
+ar[1] = confirm('вы любите кино?');
+ar[2] = confirm('это ваше первое знакомство?');
+document.write(ar);
+if(ar[0]<=18){
+    document.write('  Пошёл вон, дурак малолетний!');
+}
