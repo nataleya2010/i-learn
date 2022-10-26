009cab9cd7fea87898877091035cdb8ad09285b0
nataleya2010 <gnwr2010@gmail.com>
2022-10-26 13:47:41 +0300

    feat: new tasks

--- js/main.js
+++ js/main.js
@@ -103,15 +103,198 @@
 // console.log((explode(s)));
 
 // ! посчитать людей по количеству произведённых рукопожатий
-function getParticipants(handshakes){
-  if(handshakes){
-    if(handshakes==1){
-      return 2;
-    }else{
-      return (handshakes-1)*handshakes;
-    }
+// function getParticipants(handshakes){
+//   if(handshakes){
+//     if(handshakes==1){
+//       return 2;
+//     }else{
+//       return (handshakes-1)*handshakes;
+//     }
+//   }
+//   return 0;
+// }
+// let handshakes =3;
+// console.log(getParticipants(handshakes));
+
+// console.log( NaN || 2 || undefined );
+// console.log( NaN && 2 && undefined );
+// console.log( 1 && 2 && 3 );
+// console.log( !1 && 2 || !3 );
+// console.log( 25 || null && !3 );
+// console.log( NaN || null || !3 || undefined || 5);
+// console.log( NaN || null && !3 && undefined || 5);
+// console.log( 5 === 5 && 3 > 1 || 5);
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//   console.log('Done!')
+// }
+
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger || cola || fries === 3 || nuggets) {
+//    console.log('Done!')
+// }
+
+// let hamburger;
+// const fries = NaN;
+// const cola = 0;
+// const nuggets = 2;
+ 
+// if (hamburger && cola || fries === 3 && nuggets) {
+//    console.log('Done!')
+// }else(console.log('ops...'));
+
+
+// for(let i =5; i<=10; i++){
+//   console.log(i);
+// };
+
+// stop:for(let i=20; i>=10; i--){
+//   if(i===13){
+//     continue stop;
+//   }
+//   console.log(i);
+
+// }
+
+// for(let i=20; i>=10; i--){
+//   if(i===13){
+//     break;
+//   }
+//   console.log(i);
+
+// }
+
+// for(let i=2; i<=10; i++){
+//   if(i%2==0){
+//     console.log(i);
+
+//   }
+// }
+
+// let i = 2;
+// while(i<16){
+//   i++;
+// if(i%2===0){
+//   continue;
+// }else{
+// console.log(i);
+// }
+
+// }
+
+// for (let i = 2; i <= 16; i++) {
+//   if (i % 2 === 0) {
+//       continue;
+//   } else {
+//       console.log(i);
+//   }
+// }
+
+// const arrayOfNumbers = [];
+// let j =0;
+// for(let i =5; i<=10;i++){
+//   arrayOfNumbers[j]=i;
+//   j++;
+// }
+// console.log(arrayOfNumbers);
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for(let i=0; i<arr.length;i++){
+//   result[i]=arr[i];
+// }
+// console.log(result);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for(let i=0; i<data.length;i++){
+//   if(typeof data[i]==='number'){
+//     data[i]=data[i]*2;
+//   }else
+//   if(typeof data[i]==='string'){
+//     data[i]=data[i]+' - done';
+//   }
+// }
+// console.log(data);
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+// for(let i=0; i<data.length; i++){
+//   result[i]=data[data.length-(i+1)];
+// }
+// console.log(result);
+
+// const lines = 5;
+// let result = '*';
+//     for(let i=0; i<=lines; i++){
+//       let zeros = ' ';
+//       for(let j=0;j<=lines-1-i;j++){
+//         zeros = zeros+' ';
+//       }
+//   console.log(zeros+result);
+//   result= '*'+result+'*';
+// }
+
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+// const lines = 5;
+// let result = ' ';
+// for(let i=1; i<=lines; i++){
+//   for(let j =0; j<lines-i; j++){
+//     result=result+' ';
+//   }
+//   console.log(result+'*'+'\n');
+// }
+
+// let result='*';
+// for(let i=1;i<5;i++){
+//   result=result+'*';
+// }
+// console.log(result);
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result);
+//      *
+//     ***
+//    *****
+//   *******
+//  *********
+// ***********
+
+const lines = 5;
+let result = '';
+for(let i =0; i<=lines;i++){
+  for(let j = 0; j<lines-i; j++){
+    result +=' ';
   }
-  return 0;
+  for(let j =0; j < i*2+1; j++){
+    result +='*';
+  }
+  result +='\n';
 }
-let handshakes =3;
-console.log(getParticipants(handshakes));
+console.log(result);
